d479b5e6b55ef474e384a4ff27a6443fb935ca0b
Robin Freyler <robin.freyler@gmail.com>
2020-08-04 02:05:51 +0200

    extend functionality of BoundedStack and fix some bugs

--- src/utils/bounded_stack.rs
+++ src/utils/bounded_stack.rs
@@ -1,15 +1,22 @@
 use super::Error;
-use core::slice;
+use core::{
+    ops,
+    slice,
+};
 
 /// A stack that is bound to a given maximum size.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BoundedStack<T> {
+    capacity: usize,
     stack: Vec<T>,
 }
 
 impl<T> Default for BoundedStack<T> {
     fn default() -> Self {
-        Self { stack: Vec::default() }
+        Self {
+            capacity: 0,
+            stack: Vec::default(),
+        }
     }
 }
 
@@ -23,13 +30,20 @@ impl<T> BoundedStack<T> {
         if self.capacity() > new_cap {
             return Err(Error::InvalidSizeIncrement)
         }
-        self.stack.reserve(new_cap - self.capacity());
+        let additional = new_cap - self.capacity();
+        self.stack.reserve(additional);
+        self.capacity += additional;
         Ok(())
     }
 
     /// Returns the length of the bounded stack.
     pub fn len(&self) -> usize {
         self.stack.len()
+    }
+
+    /// Returns the capacity of the bounded stack.
+    pub fn capacity(&self) -> usize {
+        self.capacity
     }
 
     /// Returns `true` if the bounded stack is empty.
@@ -40,11 +54,6 @@ impl<T> BoundedStack<T> {
     /// Returns `true` if the bounded stack is full.
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity()
-    }
-
-    /// Returns the capacity of the bounded stack.
-    pub fn capacity(&self) -> usize {
-        self.stack.capacity()
     }
 
     /// Returns a shared reference to the last value of the stack if any.
@@ -104,6 +113,30 @@ impl<T> BoundedStack<T> {
     pub fn iter_mut(&mut self) -> slice::IterMut<T> {
         self.stack.iter_mut()
     }
+
+    /// Ensures that the given index is valid for the bounded array.
+    ///
+    /// # Errors
+    ///
+    /// If the given index is out of bounds.
+    fn ensure_valid_index(&self, index: usize) -> Result<usize, Error> {
+        if index >= self.len() {
+            return Err(Error::OutOfBoundsAccess)
+        }
+        Ok(index)
+    }
+
+    /// Returns a shared reference to the element at the given index.
+    pub fn get(&self, index: usize) -> Result<&T, Error> {
+        self.ensure_valid_index(index)
+            .map(move |index| &self.stack[index])
+    }
+
+    /// Returns an exclusive reference to the element at the given index.
+    pub fn get_mut(&mut self, index: usize) -> Result<&mut T, Error> {
+        self.ensure_valid_index(index)
+            .map(move |index| &mut self.stack[index])
+    }
 }
 
 impl<'a, T> IntoIterator for &'a BoundedStack<T> {
@@ -121,5 +154,30 @@ impl<'a, T> IntoIterator for &'a mut BoundedStack<T> {
 
     fn into_iter(self) -> Self::IntoIter {
         self.iter_mut()
+    }
+}
+
+impl<T> ops::Index<usize> for BoundedStack<T> {
+    type Output = T;
+
+    /// Returns a shared reference to the value for the given index if any.
+    ///
+    /// # Panics
+    ///
+    /// Returns an error if the index is out of bounds.
+    fn index(&self, index: usize) -> &Self::Output {
+        self.get(index).expect("encountered out of bounds index")
+    }
+}
+
+impl<T> ops::IndexMut<usize> for BoundedStack<T> {
+    /// Returns an exclusive reference to the value for the given index if any.
+    ///
+    /// # Panics
+    ///
+    /// Returns an error if the index is out of bounds.
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        self.get_mut(index)
+            .expect("encountered out of bounds index")
     }
 }
