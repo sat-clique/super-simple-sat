08f9590320552439127ab7172f5be4c67590659d
Robin Freyler <robin.freyler@gmail.com>
2020-08-17 23:41:29 +0200

    add minimal solver driver

---
+++ crates/driver/src/main.rs
@@ -0,0 +1,21 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+use std::fs;
+use solver::Solver;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(name = "input .cnf file", parse(from_os_str))]
+    input: PathBuf,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let cnf_contents = fs::read(opt.input)
+        .expect("couldn't read provided input .cnf file");
+    let mut solver = Solver::from_cnf(&mut &cnf_contents[..])
+        .expect("couldn't properly decode provided input .cnf file");
+    println!("start solving ...");
+    let result = solver.solve(vec![]);
+    println!("solution = {:?}", result);
+}
