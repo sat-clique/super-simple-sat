8c20edc62c7cb92df2ac31e2374addae1eaea460
Robin Freyler <robin.freyler@gmail.com>
2021-10-11 23:23:23 +0200

    refactor Solver::solve benchmarks

--- crates/solver/benches/solve.rs
+++ crates/solver/benches/solve.rs
@@ -1,16 +1,8 @@
-use core::{
-    fmt,
-    fmt::{
-        Display,
-        Formatter,
-    },
-};
 use criterion::{
     black_box,
     criterion_group,
     criterion_main,
     BatchSize,
-    BenchmarkId,
     Criterion,
 };
 use s3sat_solver::Solver;
@@ -65,83 +57,8 @@ where
         .collect::<Vec<_>>()
 }
 
-/// The kind of the SAT problem.
-#[derive(Debug, Copy, Clone)]
-pub enum ProblemKind {
-    /// Randomly generated 3-SAT problem instance.
-    Random3Sat,
-}
-
-impl Display for ProblemKind {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Self::Random3Sat => write!(f, "Random 3-SAT"),
-        }
-    }
-}
-
-/// The benchmark parameters.
-pub struct BenchParams {
-    problem_kind: ProblemKind,
-    satisfiable: Satisfiability,
-    len_clauses: usize,
-    len_literals: usize,
-    instance: usize,
-}
-
-/// The known satisfiability of a SAT benchmark instance.
-#[derive(Debug)]
-pub enum Satisfiability {
-    /// The benchmark instance is satisfiable.
-    Sat,
-    /// The benchmark instance is unsatisfiable.
-    Unsat,
-}
-
-impl Display for Satisfiability {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Self::Sat => write!(f, "satisfiable"),
-            Self::Unsat => write!(f, "unsatisfiable"),
-        }
-    }
-}
-
-impl BenchParams {
-    /// Creates new benchmark parameters.
-    pub fn new(
-        problem_kind: ProblemKind,
-        satisfiable: Satisfiability,
-        len_clauses: usize,
-        len_literals: usize,
-        instance: usize,
-    ) -> Self {
-        Self {
-            problem_kind,
-            satisfiable,
-            len_clauses,
-            len_literals,
-            instance,
-        }
-    }
-}
-
-impl Display for BenchParams {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} (#clauses = {}, #literals = {}, {}) #{}",
-            self.problem_kind,
-            self.len_clauses,
-            self.len_literals,
-            self.satisfiable,
-            self.instance
-        )
-    }
-}
-
 fn bench_3sat_v150_c645_sat(c: &mut Criterion) {
-    let mut g = c.benchmark_group("Solver::solve");
+    let mut g = c.benchmark_group("3sat_v150_c645 (sat)");
     g.sample_size(10);
     g.sampling_mode(criterion::SamplingMode::Flat);
     for (n, input) in collect_benchmarks_in_path("../../cnf/uf150-645/sat/")
@@ -149,9 +66,7 @@ fn bench_3sat_v150_c645_sat(c: &mut Criterion) {
         .enumerate()
     {
         let solver = Solver::from_cnf(&mut &input[..]).unwrap();
-        let param =
-            BenchParams::new(ProblemKind::Random3Sat, Satisfiability::Sat, 650, 150, n);
-        g.bench_function(BenchmarkId::from_parameter(param), |bencher| {
+        g.bench_function(n.to_string(), |bencher| {
             bencher.iter_batched_ref(
                 || solver.clone(),
                 |solver| {
@@ -165,7 +80,7 @@ fn bench_3sat_v150_c645_sat(c: &mut Criterion) {
 }
 
 fn bench_3sat_v150_c645_unsat(c: &mut Criterion) {
-    let mut g = c.benchmark_group("Solver::solve");
+    let mut g = c.benchmark_group("3sat_v150_c645 (unsat)");
     g.sample_size(10);
     g.sampling_mode(criterion::SamplingMode::Flat);
     for (n, input) in collect_benchmarks_in_path("../../cnf/uf150-645/unsat/")
@@ -173,9 +88,7 @@ fn bench_3sat_v150_c645_unsat(c: &mut Criterion) {
         .enumerate()
     {
         let solver = Solver::from_cnf(&mut &input[..]).unwrap();
-        let param =
-            BenchParams::new(ProblemKind::Random3Sat, Satisfiability::Unsat, 650, 150, n);
-        g.bench_function(BenchmarkId::from_parameter(param), |bencher| {
+        g.bench_function(n.to_string(), |bencher| {
             bencher.iter_batched_ref(
                 || solver.clone(),
                 |solver| {
